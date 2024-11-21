502a2ae462b6e30c5ebe98319ae9a8451a7e542c
Edward Mighetto <emighett@ucsc.edu>
2024-11-21 01:04:25 +0000

    Commenting plan

---
+++ scarab/src/prefetcher/best_offset/pref_bo.c
@@ -0,0 +1,14 @@
+/* 
+Struct: Recent request table 
+contains: 
+- directly mapped cache containing hashing tag bits 
+- score table 
+
+Support functions: 
+Incriment score (tag)
+- Increases the score corresponding to an item in the cache 
+
+Insert element (tage)
+- Adds new element to the cache 
+
+*/
